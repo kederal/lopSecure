605ece9162a6e399de099569d5b3404446dd3c17
kederal <87231854+kederal@users.noreply.github.com>
2023-02-10 20:26:11 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
 local gh = 'https://github.com/kederal/lopSecure/blob/main/Games/'
 
 local SupportedGames = {
-    [3183403065] = 'Anime-Adventures.lua'
+    [3183403065] = 'Anime-Adventures'
 }
 
 for i, v in pairs(SupportedGames) do
     if i == game.PlaceId or i == game.GameId then
-        loadstring(game:HttpGet(gh .. v .. '?raw=true'))()
+        loadstring(game:HttpGet(gh .. v .. '.lua'))()
     end
 end
